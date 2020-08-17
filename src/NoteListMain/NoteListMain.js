ad72ae72ed4bf20441faea16a9945a4e21091136
Amy Pepper <amyandmicahpepper@gmail.com>
2020-08-17 07:30:17 -0700

    add prop-types validation

--- src/NoteListMain/NoteListMain.js
+++ src/NoteListMain/NoteListMain.js
@@ -1,40 +1,46 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import Note from '../Note/Note'
-import CircleButton from '../CircleButton/CircleButton'
-import './NoteListMain.css'
+import React from "react";
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Note from "../Note/Note";
+import CircleButton from "../CircleButton/CircleButton";
+import PropTypes, { arrayOf } from "prop-types";
+import "./NoteListMain.css";
 
 export default function NoteListMain(props) {
   return (
-    <section className='NoteListMain'>
+    <section className="NoteListMain">
       <ul>
-        {props.notes.map(note =>
+        {props.notes.map((note) => (
           <li key={note.id}>
-            <Note
-              id={note.id}
-              name={note.name}
-              modified={note.modified}
-            />
+            <Note id={note.id} name={note.name} modified={note.modified} />
           </li>
-        )}
+        ))}
       </ul>
-      <div className='NoteListMain__button-container'>
+      <div className="NoteListMain__button-container">
         <CircleButton
           tag={Link}
-          to='/add-note'
-          type='button'
-          className='NoteListMain__add-note-button'
+          to="/add-note"
+          type="button"
+          className="NoteListMain__add-note-button"
         >
-          <FontAwesomeIcon icon='plus' />
+          <FontAwesomeIcon icon="plus" />
           <br />
           Note
         </CircleButton>
       </div>
     </section>
-  )
+  );
 }
+
+NoteListMain.propTypes = {
+  notes: arrayOf(PropTypes.object),
+};
+
+Note.propTypes = {
+  id: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+};
 
 NoteListMain.defaultProps = {
   notes: [],
-}
+};
