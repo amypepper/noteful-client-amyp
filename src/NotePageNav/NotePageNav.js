0aea56c8783aba95f23d536af8598676ca99301f
amypepper <amycarlsonpepper@gmail.com>
2020-08-28 08:01:51 -0700

    forgot to save NotePageNav

--- src/NotePageNav/NotePageNav.js
+++ src/NotePageNav/NotePageNav.js
@@ -1,47 +1,3 @@
-<<<<<<< HEAD
-import React from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import CircleButton from '../CircleButton/CircleButton'
-import ApiContext from '../ApiContext'
-import { findNote, findFolder } from '../notes-helpers'
-import './NotePageNav.css'
-
-export default class NotePageNav extends React.Component {
-  static defaultProps = {
-    history: {
-      goBack: () => { }
-    },
-    match: {
-      params: {}
-    }
-  }
-  static contextType = ApiContext;
-
-  render() {
-    const { notes, folders, } = this.context
-    const { noteId } = this.props.match.params
-    const note = findNote(notes, noteId) || {}
-    const folder = findFolder(folders, note.folderId)
-    return (
-      <div className='NotePageNav'>
-        <CircleButton
-          tag='button'
-          role='link'
-          onClick={() => this.props.history.goBack()}
-          className='NotePageNav__back-button'
-        >
-          <FontAwesomeIcon icon='chevron-left' />
-          <br />
-          Back
-        </CircleButton>
-        {folder && (
-          <h3 className='NotePageNav__folder-name'>
-            {folder.name}
-          </h3>
-        )}
-      </div>
-    )
-=======
 import React from "react";
 
 export default class NotePageNav extends React.Component {
@@ -55,6 +11,5 @@ export default class NotePageNav extends React.Component {
       );
     }
     return null;
->>>>>>> implement-design-and-routing
   }
 }
