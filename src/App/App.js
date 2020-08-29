2e0bc368559cb73e74b20bf197ad0edb6f659562
amypepper <amycarlsonpepper@gmail.com>
2020-08-29 16:48:17 -0700

    partially implement note filtering

--- src/App/App.js
+++ src/App/App.js
@@ -1,37 +1,30 @@
 import React, { Component } from "react";
 import { Route, Link } from "react-router-dom";
+
+import AddFolder from "../AddFolder/AddFolder";
+import config from "../config";
 import NoteListMain from "../NoteListMain/NoteListMain";
 import NoteListNav from "../NoteListNav/NoteListNav";
 import NotePageMain from "../NotePageMain/NotePageMain";
 import NotePageNav from "../NotePageNav/NotePageNav";
 
 import "./App.css";
-import AddFolder from "../AddFolder/AddFolder";
 
 class App extends Component {
   state = {
     folders: [],
     notes: [],
-    selected: {
-      folder: {
-        id: "b0715efe-ffaf-11e8-8eb2-f2801f1b9fd1",
-        name: "Important",
-      },
-      note: {
-        id: "cbc787a0-ffaf-11e8-8eb2-f2801f1b9fd1",
-        name: "Dogs",
-        modified: "2019-01-03T00:00:00.000Z",
-        folderId: "b0715efe-ffaf-11e8-8eb2-f2801f1b9fd1",
-        content:
-          "Corporis accusamus placeat quas non voluptas. Harum fugit molestias qui. Velit ex animi reiciendis quasi. Suscipit totam delectus ut voluptas aut qui rerum. Non veniam eius molestiae rerum quam.\n \rUnde qui aperiam praesentium alias. Aut temporibus id quidem recusandae voluptatem ut eum. Consequatur asperiores et in quisquam corporis maxime dolorem soluta. Et officiis id est quia sunt qui iste reiciendis saepe. Ut aut doloribus minus non nisi vel corporis. Veritatis mollitia et molestias voluptas neque aspernatur reprehenderit.\n \rMaxime aut reprehenderit mollitia quia eos sit fugiat exercitationem. Minima dolore soluta. Quidem fuga ut sit voluptas nihil sunt aliquam dignissimos. Ex autem nemo quisquam voluptas consequuntur et necessitatibus minima velit. Consequatur quia quis tempora minima. Aut qui dolor et dignissimos ut repellat quas ad.",
-      },
-    },
   };
 
-  filterFolders = (folderId) => {
-    return this.state.folders.filter((folder) => folder["id"] === folderId);
+  findNoteFolder = (noteId) => {
+    const selectedNote = this.findSelectedNote(noteId);
+    return this.state.folders.filter(
+      (folder) => folder.id === selectedNote.folderId
+    );
   };
-
+  findSelectedNote = (noteId) => {
+    return this.state.notes.filter((note) => note.id === noteId);
+  };
   filterNotes = (folderId) => {
     return this.state.notes.filter((note) => note["folderId"] === folderId);
   };
@@ -43,13 +36,20 @@ class App extends Component {
   };
 
   componentDidMount() {
-    fetch("http://localhost:9090/folders")
+    fetch(`${config.API_ENDPOINT}/folders`)
       .then((res) => res.json())
+      // calling the data from the API `folders` in the arg for the anony-
+      // mous func creates a var called `folders` which becomes the key
+      // inside state; since folders is already a key in the state obj,
+      // it automatically places the values stored in `folders` in that
+      // array stored in state titled `folders`
       .then((folders) => this.setState({ folders }));
     //
 
-    fetch("http://localhost:9090/notes")
+    fetch(`${config.API_ENDPOINT}/notes`)
       .then((res) => res.json())
+      // the `notes` here also replaces the `notes` array stored in state,
+      // just as it did with `folders`
       .then((notes) => this.setState({ notes }));
   }
 
@@ -66,61 +66,72 @@ class App extends Component {
             exact
             path="/"
             render={() => {
+              return <NoteListNav folders={this.state.folders} />;
+            }}
+          />
+          {/* this route's job is to render the nav sidebar that shows the
+          folder that contains the currently selected note, plus a Go Back
+          button */}
+          <Route
+            path="/note/:noteid"
+            // filter for the folder whose id matches the `folderid` of the
+            // note from the URL
+            render={(routeProps) => {
+              const selectedNote = this.findSelectedNote(
+                routeProps.match.params.noteid
+              );
+              const noteFolder = this.state.folders.filter(
+                (folder) => folder.id === selectedNote.folderId
+              );
+              return <NotePageNav folder={noteFolder} />;
+            }}
+          />
+          <Route
+            path="/folder/:folderid"
+            render={(routeProps) => {
               return (
-                <NoteListNav
-                  addFolder={(newFolder) => this.addFolder(newFolder)}
-                  folders={this.state.folders}
-                />
+                <NoteListNav {...routeProps} folders={this.state.folders} />
               );
             }}
           />
         </nav>
-        <Route
-          path="/note/:noteid"
-          render={(routeProps) => (
-            <NotePageNav {...routeProps} {...this.state} />
-          )}
-        />
-        <Route
-          path="/folder/:folderid"
-          render={(routeProps) => {
-            const activeFolder = routeProps.match.params.folderid;
-            return (
-              <NoteListNav
-                {...routeProps}
-                {...this.state}
-                addFolder={(newFolder) => this.addFolder(newFolder)}
-                activeFolder={activeFolder}
-                folders={this.state.folders}
-              />
-            );
-          }}
-        />
         <main className="App__main">
+          {/* This route's job is to render the full list of notes on the 
+          home page */}
           <Route
             exact
             path="/"
             render={() => <NoteListMain notes={this.state.notes} />}
           />
+          {/* This route's job is to render the details of a specific selected
+          note, as well as its contents */}
           <Route
             path="/note/:noteid"
-            render={(routeProps) => (
-              <NotePageMain {...routeProps} {...this.state} />
-            )}
+            render={(routeProps) => {
+              const selectedNote = this.findSelectedNote(
+                routeProps.match.params.noteid
+              );
+              return <NotePageMain note={selectedNote} />;
+            }}
           />
+          {/* This route's job is to show only the notes that have the same 
+          folderId as the folder that user has clicked on/entered */}
           <Route
             path="/folder/:folderid"
             render={(routeProps) => {
               const filteredNotes = this.filterNotes(
                 routeProps.match.params.folderid
               );
-              return <NoteListMain {...routeProps} notes={filteredNotes} />;
+              return <NoteListMain notes={filteredNotes} />;
             }}
           />
+          {/* This route's job is to take you to the form where you can create
+          a new folder that will be POSTed to the JSON server; this is the
+          only route that needs the addFolder callback prop */}
           <Route
             path="/add-folder"
-            render={(rprops) => (
-              <AddFolder {...rprops} addFolder={this.addFolder} />
+            render={(routeProps) => (
+              <AddFolder {...routeProps} addFolder={this.addFolder} />
             )}
           />
         </main>
--- src/NotePageNav/NotePageNav.js
+++ src/NotePageNav/NotePageNav.js
@@ -2,14 +2,11 @@ import React from "react";
 
 export default class NotePageNav extends React.Component {
   render() {
-    if (this.props.match.params.noteid === this.props.selected.note.id) {
-      return (
-        <div className="App__nav">
-          <button>Go back</button>
-          <div>{this.props.selected.folder.name}</div>
-        </div>
-      );
-    }
-    return null;
+    return (
+      <div className="App__nav">
+        <button>Go back</button>
+        <div>{this.props.folder.name}</div>
+      </div>
+    );
   }
 }
