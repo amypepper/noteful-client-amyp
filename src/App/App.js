cee086fc8eeaa2d887b4a54ca2dc6646b3a22278
amypepper <amycarlsonpepper@gmail.com>
2020-08-30 14:21:35 -0700

    touch up css

--- src/App/App.js
+++ src/App/App.js
@@ -55,94 +55,96 @@ class App extends Component {
       <div className="App">
         <header className="App__header">
           <Link className="link" to="/">
-            <h1>Noteful</h1>
+            <h1 className="App__header">Noteful</h1>
           </Link>
         </header>
-        <nav>
-          {/* This route's job is to render all the folders on the 
+        <div className="main-content__wrapper">
+          <nav className="App__nav">
+            {/* This route's job is to render all the folders on the 
           home page */}
-          <Route
-            exact
-            path="/"
-            render={() => {
-              return <NoteListNav folders={this.state.folders} />;
-            }}
-          />
-          {/* this route's job is to render the nav sidebar that shows the
+            <Route
+              exact
+              path="/"
+              render={() => {
+                return <NoteListNav folders={this.state.folders} />;
+              }}
+            />
+            {/* this route's job is to render the nav sidebar that shows the
           folder containing the currently selected note, plus a Go Back
           button */}
-          <Route
-            path="/note/:noteid"
-            // filter for the folder whose id matches the `folderid` of the
-            // note from the URL
-            render={(routeProps) => {
-              // grab the note based on the noteid in the URL
-              const selectedNote = this.findSelectedNote(
-                routeProps.match.params.noteid
-              );
-              // sort through folders for the id that matches
-              // `selectedNote`'s folder's id
-              const noteFolder = this.state.folders.filter((folder) => {
-                // but first, check that selectedNote[0] exists because
-                // it will be empty when render is first called
-                if (selectedNote[0]) {
-                  return folder.id === selectedNote[0].folderId;
-                }
-                return null;
-              });
-              return <NotePageNav folder={noteFolder} />;
-            }}
-          />
-          {/* This route's job is to render the full list of folders for
-          navigation purposes, when user is viewing the contents of a
+            <Route
+              path="/note/:noteid"
+              // filter for the folder whose id matches the `folderid` of the
+              // note from the URL
+              render={(routeProps) => {
+                // grab the note from state based on the noteid in the URL
+                const selectedNote = this.findSelectedNote(
+                  routeProps.match.params.noteid
+                );
+                // sort through folders for the id that matches the
+                // `selectedNote` object's folderId key
+                const noteFolder = this.state.folders.filter((folder) => {
+                  // but first, check that selectedNote[0] exists because
+                  // it will be empty when render is first called
+                  if (selectedNote[0]) {
+                    return folder.id === selectedNote[0].folderId;
+                  }
+                  return null;
+                });
+                return <NotePageNav folder={noteFolder} />;
+              }}
+            />
+            {/* This route's job is to render the full list of folders for
+          navigation purposes; renders when user is viewing the contents of a
           specific folder */}
-          <Route
-            path="/folder/:folderid"
-            render={() => {
-              return <NoteListNav folders={this.state.folders} />;
-            }}
-          />
-        </nav>
-        <main className="App__main">
-          {/* This route's job is to render the full list of notes on the 
+            <Route
+              path="/folder/:folderid"
+              render={() => {
+                return <NoteListNav folders={this.state.folders} />;
+              }}
+            />
+          </nav>
+          <main className="App__main">
+            {/* This route's job is to render the full list of notes on the 
           home page */}
-          <Route
-            exact
-            path="/"
-            render={() => <NoteListMain notes={this.state.notes} />}
-          />
-          {/* This route's job is to render the details of a specific selected
+            <Route
+              exact
+              path="/"
+              render={() => <NoteListMain notes={this.state.notes} />}
+            />
+            {/* This route's job is to render the details of a specific selected
           note, as well as its contents */}
-          <Route
-            path="/note/:noteid"
-            render={(routeProps) => {
-              const selectedNote = this.findSelectedNote(
-                routeProps.match.params.noteid
-              );
-              return <NotePageMain note={selectedNote} />;
-            }}
-          />
-          {/* This route's job is to show only the notes that have the same 
+            <Route
+              path="/note/:noteid"
+              render={(routeProps) => {
+                const selectedNote = this.findSelectedNote(
+                  routeProps.match.params.noteid
+                );
+                return <NotePageMain note={selectedNote} />;
+              }}
+            />
+            {/* This route's job is to show only the notes that have the same 
           folderId as the folder that user has clicked on/entered */}
-          <Route
-            path="/folder/:folderid"
-            render={(routeProps) => {
-              const filteredNotes = this.filterNotes(
-                routeProps.match.params.folderid
-              );
-              return <NoteListMain notes={filteredNotes} />;
-            }}
-          />
-          {/* This route's job is to take you to the form where you can create
+            <Route
+              path="/folder/:folderid"
+              render={(routeProps) => {
+                const filteredNotes = this.filterNotes(
+                  routeProps.match.params.folderid
+                );
+                return <NoteListMain notes={filteredNotes} />;
+              }}
+            />
+            {/* This route's job is to take you to the form where you can create
           a new folder that will be POSTed to the JSON server; this is the
           only route that needs the addFolder callback prop */}
-          <Route
-            path="/add-folder"
-            render={(routeProps) => (
-              <AddFolder {...routeProps} addFolder={this.addFolder} />
-            )}
-          />
-        </main>
+            <Route
+              path="/add-folder"
+              render={(routeProps) => (
+                <AddFolder {...routeProps} addFolder={this.addFolder} />
+              )}
+            />
+          </main>
+        </div>
       </div>
     );
   }
