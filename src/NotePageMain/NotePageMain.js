8ed4f82ede8f582da9f76cbfdec0bc57a554ccbc
amypepper <amycarlsonpepper@gmail.com>
2020-09-26 15:00:02 -0700

    before create new branch

--- src/NotePageMain/NotePageMain.js
+++ src/NotePageMain/NotePageMain.js
@@ -1,5 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
 import NoteDetails from "../NoteDetails/NoteDetails";
 
@@ -7,21 +8,17 @@ export default function NotePageMain(props) {
   return (
     <>
       <section>
-        <NoteDetails note={props.note} history={props.history} />
-      </section>
-      <section>
         <p>{props.note.content}</p>
       </section>
+      <section>
+        <NoteDetails note={props.note} history={props.history} />
+      </section>
+      <Link to="/add-note">
+        <button>Update Note</button>
+      </Link>
     </>
   );
 }
-
-// NotePageMain.defaultProps = {
-//   note: {
-//     content: "",
-//   },
-//   history: {},
-// };
 
 NotePageMain.propTypes = {
   note: PropTypes.shape({
